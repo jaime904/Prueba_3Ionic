d0f99e1820cc6a5a995b1d9c051960acd2157ba8
DonYosef <josi.munoz@duocuc.cl>
2023-11-25 18:47:42 -0300

    Plugin Notificacion Local funcionando!

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalNotifications, ScheduleOptions } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-tab1',
@@ -9,4 +10,23 @@ export class Tab1Page {
 
   constructor() {}
 
+  async scheduleNotification() {
+    let options: ScheduleOptions = {
+      notifications: [
+        {
+          id: 111,
+          title: "Notificacion 1",
+          body: "Esta es la primera notificacion", //esto no aparece
+          largeBody: "Esta es la primera notificacion!!",
+          summaryText: "Exito!!"
+        }
+      ]
+    }
+    try{
+      await LocalNotifications.schedule(options);
+    }
+    catch(ex){
+      alert(JSON.stringify(ex));
+    }
+  }
 }
