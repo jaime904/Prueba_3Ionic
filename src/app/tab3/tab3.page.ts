08f49fd7a92f07d5a0d20fa8fe4de76bcf47655e
jaime904 <jai.aravena@duocuc.cl>
2023-11-25 16:58:31 -0300

    Se implementa la Geolocalitatio

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ToastController } from '@ionic/angular';
+import { Geolocation } from '@capacitor/geolocation';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(
+    public toastController: ToastController,
+  ) {}
+
+   async obtenerMiUbicacion() {
+    let ubicación = await Geolocation.getCurrentPosition();
+    let ubicacionTexto = `Mi ubicación es: Latitud: ${ubicación.coords.latitude}, Longitud: ${ubicación.coords.longitude}`;
+    this.presentToast(ubicacionTexto);
+    //mostramos el mensaje de ubicación con el toast
+   }
+  
+   async presentToast( mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 1500,
+      position: 'top',
+    });
+
+    await toast.present();
+  }
+  
+  
+  
+  
+  
 
 }
